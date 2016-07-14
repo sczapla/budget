31c9d8a6e3ee469d3968e608475fe1f04aaacbf4
Tomasz Wójcicki <tomasz.wojcicki@cashbill.pl>
2016-07-14 09:36:16 +0200

    logout

--- src/main/java/com/budget/controllers/IndexController.java
+++ src/main/java/com/budget/controllers/IndexController.java
@@ -1,7 +1,13 @@
 package com.budget.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -29,6 +35,15 @@ public class IndexController {
 	@RequestMapping("/login")
 	public String login() {
 		return "pages-login";
+	}
+
+	@RequestMapping(value = "/logout", method = RequestMethod.GET)
+	public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (auth != null) {
+			new SecurityContextLogoutHandler().logout(request, response, auth);
+		}
+		return "redirect:/login?logout";
 	}
 
 	@RequestMapping("/register")
